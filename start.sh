0d0bdcb39f0c490ad171962a5fd6f3667b0dcba6
Giovanni Bassi <giggio@giggio.net>
2023-12-28 13:01:49 -0300

    Add config write to start.sh

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 k3d cluster start knativetest
+k3d kubeconfig write knativetest --output "$HOME"/.kube/knativetest
 "$DIR"/proxy/start.sh
